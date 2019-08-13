dd55aa57b13aa971b87cad8b135a58715afb21f9
Rohit Gohri <gohri.rohit@gmail.com>
2019-08-14 00:20:10 +0530

    Move init blog fn outside

--- blog.js
+++ blog.js
@@ -8,6 +8,58 @@ const jsdom = require("jsdom").JSDOM,
 const { updateHTML } = require("./populate");
 const { getConfig, getBlog, updateBlog, outDir } = require("./utils");
 
+async function initBlogFiles(folder, blog_data, conf) {
+  const blogPath = `${outDir}/blog/${folder}/index.html`;
+  await fs.copyFileAsync(
+    `${__dirname}/assets/blog/blogTemplate.html`,
+    blogPath
+  );
+  const dom = await jsdom.fromFile(blogPath, options);
+  const window = dom.window;
+  const document = window.document;
+
+  const icon = document.createElement("link");
+  icon.setAttribute("rel", "icon");
+  icon.setAttribute("href", conf[0].userimg);
+  icon.setAttribute("type", "image/png");
+  document.getElementsByTagName("head")[0].appendChild(icon);
+
+  document.getElementById(
+    "profile_img_blog"
+  ).style.background = `url('${conf[0].userimg}') center center`;
+
+  document.getElementById("username_blog").innerHTML = `
+    <span style="display:${!conf[0].name ? "none" : "block"};">
+      ${conf[0].name}
+    </span>
+    <br>@${conf[0].username}
+    <br><b id="blog_time" data-iso="${blog_data.created_at.toISOString()}">
+        ${blog_data.created_at.toLocaleDateString()}
+    </b>`;
+
+  if ((conf[0].theme = "dark.css")) {
+    document.querySelector("#background_overlay").style.background =
+      "linear-gradient(0deg, rgba(10, 10, 10, 1), rgba(10, 10, 10, 0.1))";
+  } else {
+    document.querySelector("#background_overlay").style.background =
+      "linear-gradient(0deg, rgba(255, 255, 255, 1), rgba(255, 255, 255, 0.1))";
+  }
+
+  document.getElementsByTagName("title")[0].textContent = blog_data.page_title;
+  document.getElementById("blog_title").textContent = blog_data.title;
+  document.getElementById("blog_sub_title").textContent = blog_data.sub_title;
+
+  await fs.writeFileAsync(
+    blogPath,
+    "<!DOCTYPE html>" + window.document.documentElement.outerHTML
+  );
+
+  if (!fs.existsSync(`${outDir}/blog/${folder}/index.md`)) {
+    // Create empty md file
+    await fs.writeFileAsync(`${outDir}/blog/${folder}/index.md`, "");
+  }
+}
+
 async function createBlog(
   title,
   { subtitle, pagetitle, folder, image, update = false } = {}
@@ -33,6 +85,7 @@ async function createBlog(
     url_title: folder,
     title: title,
     sub_title: subtitle,
+    page_title: pagetitle,
     top_image:
       image ||
       "https://images.unsplash.com/photo-1553748024-d1b27fb3f960?w=1450",
@@ -51,51 +104,7 @@ async function createBlog(
     return;
   }
 
-  const blogPath = `${outDir}/blog/${folder}/index.html`;
-  await fs.copyFileAsync(
-    `${__dirname}/assets/blog/blogTemplate.html`,
-    blogPath
-  );
-  const dom = await jsdom.fromFile(blogPath, options);
-  const window = dom.window;
-  const document = window.document;
-
-  const icon = document.createElement("link");
-  icon.setAttribute("rel", "icon");
-  icon.setAttribute("href", conf[0].userimg);
-  icon.setAttribute("type", "image/png");
-  document.getElementsByTagName("head")[0].appendChild(icon);
-
-  document.getElementById(
-    "profile_img_blog"
-  ).style.background = `url('${conf[0].userimg}') center center`;
-
-  document.getElementById("username_blog").innerHTML = `<span style="display:${
-    !conf[0].name ? "none" : "block"
-  };">
-      ${conf[0].name}
-    </span>
-    <br>@${conf[0].username}
-    <br><b id="blog_time" data-iso="${blog_data.created_at.toISOString()}">
-        ${blog_data.created_at.toLocaleDateString()}
-    </b>`;
-
-  if ((conf[0].theme = "dark.css")) {
-    document.querySelector("#background_overlay").style.background =
-      "linear-gradient(0deg, rgba(10, 10, 10, 1), rgba(10, 10, 10, 0.1))";
-  } else {
-    document.querySelector("#background_overlay").style.background =
-      "linear-gradient(0deg, rgba(255, 255, 255, 1), rgba(255, 255, 255, 0.1))";
-  }
-
-  document.getElementsByTagName("title")[0].textContent = pagetitle;
-  document.getElementById("blog_title").textContent = title;
-  document.getElementById("blog_sub_title").textContent = subtitle;
-
-  await fs.writeFileAsync(
-    `${outDir}/blog/${folder}/index.html`,
-    "<!DOCTYPE html>" + window.document.documentElement.outerHTML
-  );
+  await initBlogFiles(folder, blog_data, conf);
 
   if (conflicting.length) {
     old_blogs.forEach(blog => {
